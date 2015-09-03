dd86257ae4e84c489a17d8a8431a829db8a5ab63
Yayoi Wesp <ls-micro@ya.ru>
2015-09-03 12:23:47 +0300

    update

--- VS/Program.cs
+++ VS/Program.cs
@@ -1,4 +1,12 @@
-﻿using Rc.Framework;
+﻿// =========================================================================//==============================================================//
+//                                                                          //                                                              //
+//                                                                          //             Copyright © Of Fire Twins Wesp 2015              //
+// Author= {"Callada", "Another"}                                           //                                                              //
+// Project="Soul.Language"                                                  //                  Alise Wesp & Yuuki Wesp                     //
+// Version File="1.0"                                                       //                                                              //
+// License="root\\LICENSE", LicenseType="MIT"                               //                                                              //
+// =========================================================================//==============================================================//
+using Rc.Framework;
 using Soul.Core.Rex;
 using System;
 using System.Collections.Generic;
@@ -15,17 +23,16 @@ namespace SSBuild
         internal static string headerConsole = "+g4 Soul+g0 ";
         internal static string versionBuild = "1.4";
 
-        internal static List<string> IncludeAssembly = new List<string>();
         static void Main(string[] args)
         {
             //! Write Header SSBuild
             Terminal.WriteLine($"Version:{versionBuild}", headerConsole);
             Terminal.WriteLine($"Soul Sharp Builder.", headerConsole);
             Terminal.WriteLine($"Support +g6 Runtime+g0  Platform: net.core +g2 4.0+g0 , soul.core +g2 1.0+g0 .", headerConsole);
-            Terminal.WriteLine($"Copyright (C) Of Fire Twins Wesp 2015", headerConsole);
+            Terminal.WriteLine($"Copyright (С) Близницы Весп (Twins Wesp), 2015 г.", headerConsole);
             //! End Write Header
-            
-            Regex r = new RegexBase().rex_method;
+
+
             string sdahd =
                 @"
                 #include <Soul.dll>
